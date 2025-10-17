8fa3fadd4583893cbbf6432633c57a14e5cebcba
sourabh <sourabhbehera916@gmail.com>
2025-10-17 13:18:47 +0530

    test4

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",                // Path to your .feature files
-        glue = {"stepDefination", "config"},                     // Package containing step definitions
+        glue = {"stepDefination"},                     // Package containing step definitions
         plugin = {
                 "pretty",                                            // Console output
                 "html:target/cucumber-report.html",                  // HTML report
